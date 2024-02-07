ea7d63544ddebdd0f91123a69479387e8d6271e1
Corpsey <102923473+Corp69@users.noreply.github.com>
2024-02-06 22:47:03 -0600

    terminamos el lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/login.component'),
+    children: [
+      {
+        path: 'login',
+        title: 'Login',
+        loadComponent: () => import('./auth/login/login.component'),
+      },
+      {
+        path: '', redirectTo: 'login', pathMatch: 'full',
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  }
+
+];
