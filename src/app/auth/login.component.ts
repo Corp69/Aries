efe5c84ae16cb4455d55becb83f6b19602a8b441
Corpsey <102923473+Corp69@users.noreply.github.com>
2024-02-07 00:05:22 -0600

    terminamos un side bar

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule,  SidebarModule, ButtonModule ],
   templateUrl: './login.component.html',
   styles: ``
 })
 
 export default class LoginComponent {
+
+  sidebarVisible: boolean = true;
 
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, SidebarModule, ButtonModule  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 
 export default class LoginComponent {
+
+
+  sidebarVisible: boolean = true;
 
 }
