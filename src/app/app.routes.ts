31457227482c28e1af2795465bbd54ef0e5eb8c2
Corpsey69 <ing.elizandro.casillasgmail.com>
2024-02-14 21:03:49 -0600

    rutas e instalaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: 'change-detection',
+        title: 'Change Detection',
+        loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component'),
+      }, 
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+
+
+
+
+
+];
