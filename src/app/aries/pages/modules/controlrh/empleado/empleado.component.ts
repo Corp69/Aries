6bb873e5a39d9229baced3154407f0e6af12b09b
elizandro.carballo <elizandro.carballo@ids.com.mx>
2024-04-08 09:32:47 -0600

    trasladamos la info a mi pc

--- src/app/aries/pages/modules/controlrh/empleado/empleado.component.ts
+++ src/app/aries/pages/modules/controlrh/empleado/empleado.component.ts
@@ -4,15 +4,15 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // modelos
 import { MdlEmpleado } from './models/MdlEmpleado';
-// shared 
+// shared
 import { CargaComponent } from '@shared/pages/modales/carga/carga.component';
 import { ConfirmacionComponent } from '@shared/pages/modales/confirmacion/confirmacion.component';
 import { NombresComponent } from '@shared/pages/busquedas/nombres/nombres.component';
-// servicios 
+// servicios
 import { EmpleadoService } from './Services/Empleado.service';
 //Interface
 import { ConfirmacionMensaje, listados } from './interface/empleado';
-// prime NG 
+// prime NG
 import { DividerModule } from 'primeng/divider';
 import { MessagesModule} from 'primeng/messages';
 import { MessageModule} from 'primeng/message';
@@ -43,14 +43,14 @@ import { DropdownModule } from 'primeng/dropdown';
     CargaComponent,
     ConfirmacionComponent,
     NombresComponent,
-    //prime NG 
+    //prime NG
     KeyFilterModule,
     InputGroupModule,
     InputGroupAddonModule,
     DividerModule,
     MessagesModule,
     MessageModule,
-    DropdownModule, 
+    DropdownModule,
     CardModule,
     ProgressSpinnerModule,
     DialogModule,
@@ -67,19 +67,19 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
   //==============================================================================================================
   // Modal: mensaje Confirmacion falso para no cargar la modal
   public ConfirmacionMdl: boolean = false;
-  // variables para mensaje actualizar guardar 
+  // variables para mensaje actualizar guardar
   public ConfirmacionMsjMdl: ConfirmacionMensaje = { msjTipo: 1, titulo: '', mensaje: '', detalle: '' };
 
   //==============================================================================================================
   // Modal: mensaje bc-nombreMdl falso para no mostrar la modal true para mostrar
   public bcNombreMdl: boolean = false;
-  // variables para enviar a modal enviar y realizar busqueda 
+  // variables para enviar a modal enviar y realizar busqueda
   public bcnombreDataMdl: ConfirmacionMensaje = { msjTipo: 1, titulo: '', mensaje: '', detalle: '' };
 
   // variable que bloquea la vista
   public Ariesblocked: boolean  = false;
   //=============================================================================================================
-  // satMDL 
+  // satMDL
   public tabalaSat1: String = 'sat_usocfdi';
   public usoCFDI = ''
 
@@ -96,7 +96,7 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
   // Listados:
   public lstestatus:        listados[] = [];
   public listEmpleadoTipo:  listados[] = [];
-    
+
   //public lstProveedorClasificacion: any;
   //  public lstProovedorOperacion: any;
   //==============================================================================================================
@@ -108,8 +108,8 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
   public frmEmpleado: FormGroup = this.fb.group({
     id:     [-1],
     nombre: [, [Validators.required, Validators.minLength(3)]],
-    paterno: [, [Validators.required, Validators.minLength(3)]],
-    materno: [, [Validators.required, Validators.minLength(3)]],
+    apellidoP: [, [Validators.required, Validators.minLength(3)]],
+    apellidoM: [, [Validators.required, Validators.minLength(3)]],
     codigo: [],
     correo: [],
     rfc:    [, [Validators.required, Validators.minLength(3)]],
@@ -122,7 +122,7 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
     contacto2: [, [Validators.required, Validators.minLength(3)]],
     nss: [, [Validators.required, Validators.minLength(3)]],
     imagen: [null],
-    id_rh_estatus: [null],
+    id_estatus: [null],
     id_tipo:    [null],
     id_rh_empleado: [parseInt(localStorage.getItem("id"))],
     id_sat_usocfdi: [1],
@@ -138,13 +138,13 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
   });
 
   /**
-   * 
-   * @param route 
-   * @param fb 
+   *
+   * @param route
+   * @param fb
    * @param servicio
-   * 
+   *
     //private datePipe: DatePipe,
-   *  
+   *
    */
   constructor( private route: ActivatedRoute, private fb: FormBuilder, private servicio: EmpleadoService ) {}
 
@@ -168,8 +168,8 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
          // seteamos la informacion
          this.frmEmpleado.controls['id'].setValue(resp.Detalle.id);
          this.frmEmpleado.controls['nombre'].setValue(resp.Detalle.nombre);
-         this.frmEmpleado.controls['paterno'].setValue(resp.Detalle.paterno);
-         this.frmEmpleado.controls['materno'].setValue(resp.Detalle.materno);
+         this.frmEmpleado.controls['apellidoP'].setValue(resp.Detalle.apellidoP);
+         this.frmEmpleado.controls['apellidoM'].setValue(resp.Detalle.apellidoM);
          this.frmEmpleado.controls['rfc'].setValue(resp.Detalle.rfc);
          this.frmEmpleado.controls['codigo'].setValue(resp.Detalle.codigo);
          this.frmEmpleado.controls['curp'].setValue(resp.Detalle.curp);
@@ -183,8 +183,8 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
          this.frmEmpleado.controls['contacto2'].setValue(resp.Detalle.contacto2);
          this.frmEmpleado.controls['nss'].setValue(resp.Detalle.nss);
          this.frmEmpleado.controls['imagen'].setValue(resp.Detalle.imagen);
-         // asignamos el valor String debido a que no es int los parsearemos a String 
-         this.frmEmpleado.controls['id_rh_estatus'].setValue(resp.Detalle.id_estatus.toString());
+         // asignamos el valor String debido a que no es int los parsearemos a String
+         this.frmEmpleado.controls['id_estatus'].setValue(resp.Detalle.id_estatus.toString());
          this.frmEmpleado.controls['id_tipo'].setValue(resp.Detalle.id_tipo.toString());
          this.frmEmpleado.controls['id_rh_empleado'].setValue(parseInt(resp.Detalle.id_rh_empleado));
          this.frmEmpleado.controls['fecha_nacimiento'].setValue(resp.Detalle.fecha_nacimiento.toString());
@@ -196,7 +196,7 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
         });
         //CARGAMOS CFDI
         this.servicio.Datacfdi(+params['id']).subscribe(resp => {
-          // rellenamos los campos de CFDI EN UNA CONSULTA APARTE  
+          // rellenamos los campos de CFDI EN UNA CONSULTA APARTE
           this.frmEmpleado.controls['id_sat_regimenfiscal'].setValue(parseInt(resp.Detalle.proveedorcfdi.id_sat_regimenfiscal));
           this.frmEmpleado.controls['id_sat_regimenfiscal'].setValue(parseInt(resp.Detalle.proveedorcfdi.id_sat_usocfdi));
           this.usoCFDI = resp.Detalle.proveedorcfdi.usocfdi;
@@ -217,7 +217,7 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
     this.ConfirmacionMdl  = false;
     // bloqueamos el boton
     this.BtnSpinner   = true;
-    // bloqueamos pantalla 
+    // bloqueamos pantalla
     this.Ariesblocked = true;
     //===============================
     this.servicio.AlmacenarProveedor(this.frmEmpleado.value).subscribe(resp => {
@@ -226,8 +226,8 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
           //============================================================
           this.ConfirmacionMsjMdl.msjTipo = 2; //resp.IdMensj;
           this.ConfirmacionMsjMdl.titulo  = "Aries: Info"; //resp.Titulo;
-          this.ConfirmacionMsjMdl.mensaje = resp.Mensaje; 
-          this.ConfirmacionMsjMdl.detalle = resp.Solucion; 
+          this.ConfirmacionMsjMdl.mensaje = resp.Mensaje;
+          this.ConfirmacionMsjMdl.detalle = resp.Solucion;
           this.ConfirmacionMdl = true;
           // desbloqueamos la pantalla
           this.Ariesblocked = false;
@@ -236,26 +236,26 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
           //============================================================
           this.ConfirmacionMsjMdl.msjTipo = resp.IdMensj;
           this.ConfirmacionMsjMdl.titulo  = 'Aries: Info'; //resp.Titulo;
-          this.ConfirmacionMsjMdl.mensaje = resp.Mensaje; 
+          this.ConfirmacionMsjMdl.mensaje = resp.Mensaje;
           this.ConfirmacionMsjMdl.detalle = resp.Detalle;
-           // mostramos el resultado de la informacion  
-           this.ConfirmacionMdl  = true;  
+           // mostramos el resultado de la informacion
+           this.ConfirmacionMdl  = true;
           // desbloqueamos la pantalla
-          this.Ariesblocked = false; 
+          this.Ariesblocked = false;
           break;
         default:
           //============================================================
           this.ConfirmacionMsjMdl.msjTipo = resp.IdMensj;
           this.ConfirmacionMsjMdl.titulo  = 'Aries: Info'; //resp.Titulo;
-          this.ConfirmacionMsjMdl.mensaje = resp.Mensaje; 
-          this.ConfirmacionMsjMdl.detalle = resp.Detalle; 
+          this.ConfirmacionMsjMdl.mensaje = resp.Mensaje;
+          this.ConfirmacionMsjMdl.detalle = resp.Detalle;
           //============================================================
-          // instanciamos elmismo formulario para actualizar 
+          // instanciamos elmismo formulario para actualizar
           this.frmEmpleado.setValue(this.frmEmpleado.value);
           // agregamos el ID para generar la actualizacion
           this.frmEmpleado.controls['id'].setValue(parseInt(resp.Id));
-          // mostramos el resultado de la informacion  
-          this.ConfirmacionMdl  = true;  
+          // mostramos el resultado de la informacion
+          this.ConfirmacionMdl  = true;
           // desbloqueamos la pantalla
           this.Ariesblocked     = false;
           break;
@@ -266,7 +266,7 @@ export default class EmpleadoComponent implements OnInit, AfterViewInit  {
 
   // metodo para agregar un nuevo proveedor
   public NuevoEmpleado = () => {
-    // reiniciamos el formulario 
+    // reiniciamos el formulario
     this.frmEmpleado.setValue(this.MdlEmpleado);
     // prime trabaja con String lo pasamos a String el valor numerico
     this.frmEmpleado.controls['id_estatus'].setValue("1");
--- src/app/aries/pages/modules/controlrh/empleado/models/MdlEmpleado.ts
+++ src/app/aries/pages/modules/controlrh/empleado/models/MdlEmpleado.ts
@@ -1,15 +1,15 @@
 export class MdlEmpleado
-{	
-	public id: number = -1; 
-	public nombre: String = ''; 
-	public paterno: String = '';
-	public materno: String = '';
+{
+	public id: number = -1;
+	public nombre: String = '';
+	public apellidoP: String = '';
+	public apellidoM: String = '';
 	public correo: String = '';
 	public nss: String = '';
 	public rfc: String = '';
 	public curp: String = '';
 	public fecha_nacimiento:  Date = new Date();
-	public fecha_ingreso:  Date = new Date();	
+	public fecha_ingreso:  Date = new Date();
 	public cuenta_banco: number = 0;
 	public clabe: number = 0;
 	public whatsapp: number = 0;
@@ -19,7 +19,7 @@ export class MdlEmpleado
 	public contacto2: String = '';
 
 	public id_sexo: number = -1;
-	public id_rh_estatus: number = -1;
+	public id_estatus: number = -1;
 	public id_rh_clasificacion: number = -1;
 	public id_rh_grado: number = -1;
 	public id_rh_puesto: number = -1;
