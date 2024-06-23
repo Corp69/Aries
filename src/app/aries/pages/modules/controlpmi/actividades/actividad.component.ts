9111f8b336b2e4ca7b5abe03354d614b4a27efe7
ElyCasillass <ht@dochmart.com>
2024-06-23 02:22:26 -0600

    Maquetanos el diseño de PMI

--- src/app/aries/pages/modules/controlpmi/actividades/actividad.component.ts
+++ src/app/aries/pages/modules/controlpmi/actividades/actividad.component.ts
@@ -1,12 +1,112 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// prime NG
+import { DividerModule } from 'primeng/divider';
+import { MessageModule} from 'primeng/message';
+import { ProgressSpinnerModule} from 'primeng/progressspinner';
+import { CardModule} from 'primeng/card';
+import { DialogModule} from 'primeng/dialog';
+import { TooltipModule} from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { GenericaComponent } from '@shared/pages/busquedas/generica/generica.component';
+import { BlockUIModule } from 'primeng/blockui';
+import { DropdownModule } from 'primeng/dropdown';
+import {ToastModule} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
+import { ActividadService } from './Services/actividad.service';
+//servicio 
+
 
 @Component({
   selector: 'app-actividad',
   standalone: true,
-  imports: [],
+  imports: [
+    
+    //angular
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+
+        //prime NG
+        KeyFilterModule,
+        InputGroupModule,
+        InputGroupAddonModule,
+        DividerModule,
+        MessageModule,
+        DropdownModule,
+        CardModule,
+        ProgressSpinnerModule,
+        DialogModule,
+        TooltipModule,
+        ButtonModule,
+        RippleModule,
+        BlockUIModule,
+        DividerModule,
+        ToastModule,
+
+ ],
+ providers: [
+   MessageService
+ ],
   templateUrl: './actividad.component.html',
   styleUrl: './actividad.component.scss'
 })
 export default class ActividadComponent {
+//==============================================================================================================
+  //modelos:
+  //public MdlProyecto: MdlProyecto = new MdlProyecto();
+ 
+   // variable que bloquea la vista
+   public Ariesblocked: boolean  = false;
+  
+   // bloqueamos el boton
+  BtnSpinner: boolean  = false;
+  
+  //==============================================================================================================
+  //Formularios del app:
+  public frm: FormGroup = this.fb.group({
+    id:     [-1],
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    codigo: [],
+    correo: [],
+    rfc:    [, [Validators.required, Validators.minLength(3)]],
+    curp:   [],
+    id_estatus: [null],
+    id_tipo:    [null],
+    id_rh_empleado: [parseInt(localStorage.getItem("id"))],
+    id_sat_usocfdi: [1],
+    //id_sat_doc_cobro:           [1],
+    id_sat_regimenfiscal: [1],
+    imagen: [null]
+  });
 
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private servicio: ActividadService )
+    {}
+
+
+    public Nuevo(){
+
+    }
+   
+    public Almacenar(){
+
+    }
+
+    public lst(){
+
+    }
 }
+
---
+++ src/app/aries/pages/modules/controlpmi/proyectos/models/MdlProyecto.ts
@@ -0,0 +1,10 @@
+export class MdlProyecto
+{
+public id:             number = -1;
+public id_estatus:     number = 1;
+public nombre:         String = "";
+public problematica:   String = "";
+public objetivo:       String = "";
+public alcance:        String = "";
+
+}
