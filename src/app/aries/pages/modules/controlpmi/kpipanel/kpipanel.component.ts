d3cdc481ba3f6e69309c457a5c82702412a5783f
unknown <devingric@gmail.com>
2024-07-30 20:24:30 -0600

    cambios en kpi

--- src/app/aries/pages/modules/controlpmi/kpipanel/kpipanel.component.ts
+++ src/app/aries/pages/modules/controlpmi/kpipanel/kpipanel.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {ToastModule} from 'primeng/toast';
+import { BlockUIModule } from 'primeng/blockui';
+import {CardModule} from 'primeng/card';
+import { TooltipModule } from 'primeng/tooltip';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-kpipanel',
   standalone: true,
-  imports: [],
+  imports: [
+    ToastModule,
+    BlockUIModule,
+    CardModule,
+    TooltipModule
+  ],
+  providers: [MessageService],
   templateUrl: './kpipanel.component.html',
   styleUrl: './kpipanel.component.scss'
 })
+
+
 export default class KpipanelComponent {
 
+      // variable que bloquea la vista
+      public Ariesblocked: boolean  = false;
+
+
+public Navegar(){
+
 }
+
+}
+
+
