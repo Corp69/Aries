85f649879d87650192c8c1086ec18ecb4b85587b
ElyCasillass <ht@dochmart.com>
2024-08-18 03:36:51 -0600

    Agregamos la cita en espacio publico

---
+++ src/app/public/membresias/citas/dentista/Services/Cita.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MdlCita } from '../models/MdlCita';
+import { ErroresService } from '@shared/errores.service';
+import { environment } from '../../../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CitaService {
+  constructor(private http: HttpClient, private errores: ErroresService) { }
+
+  public NuevaCita( data: MdlCita): Observable<any> {
+    let headers = new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    });
+    return this.http.post(`${environment.baseUrl}clientes/ctr/schema`,
+      {
+        "ExSchema": "membresias",
+        "funcion": "insert_cita_consecutiva",
+        "data": {
+          "_descripcion": data.descripcion,
+          "_fechainicio": data.fecha_inicio,
+          "_fechafinal": data.fecha_final
+        }
+      }
+      ,
+      { headers: headers }
+    ).pipe(
+      catchError((error) => {
+        return throwError(this.errores.getErrores(error));
+      })
+    );
+  }
+  // ? ==================================================================================
+
+
+}
---
+++ src/app/public/membresias/citas/dentista/models/MdlCita.ts
@@ -0,0 +1,8 @@
+export class MdlCita
+{
+    public id:              number = -1;
+    public descripcion:     String = "";
+    public fecha_inicio:    String = "";
+    public fecha_final:     String = "";
+    
+}
