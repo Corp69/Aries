6a5884964207f150c5e9ed07b6a2e59570812d07
Corpsey69 <ELIZANDRO@GMAIL.COM>
2024-10-15 11:06:08 -0600

    Agregamos el form y grid de sucursales

---
+++ src/app/aries/pages/modules/controlapp/extensiones/grid-extensiones/grid-extensiones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-extensiones',
+  standalone: true,
+  imports: [],
+  templateUrl: './grid-extensiones.component.html',
+  styleUrl: './grid-extensiones.component.scss'
+})
+export class GridExtensionesComponent {
+
+}
